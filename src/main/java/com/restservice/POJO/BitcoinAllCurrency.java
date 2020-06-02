467ac1797738f5b54ebbfe280fc9c6d4d678be7a
John <johnathonmonson@gmail.com>
2020-06-02 13:51:56 -0700

    Added support to fetch all currencies with default endpoint

--- src/main/java/com/restservice/POJO/BitcoinAllCurrency.java
+++ src/main/java/com/restservice/POJO/BitcoinAllCurrency.java
@@ -1,31 +1,353 @@
 package com.restservice.POJO;
 
 public class BitcoinAllCurrency {
-    private String disclaimer;
     private String rateUSD;
     private String rateEUR;
     private String rateGBP;
+    private String rateCAD;
+    private String rateHKD;
+    private String rateISK;
+    private String ratePHP;
+    private String rateDKK;
+    private String rateHUF;
+    private String rateCZK;
+    private String rateAUD;
+    private String rateRON;
+    private String rateSEK;
+    private String rateIDR;
+    private String rateINR;
+    private String rateBRL;
+    private String rateRUB;
+    private String rateHRK;
+    private String rateJPY;
+    private String rateTHB;
+    private String rateCHF;
+    private String rateSGD;
+    private String ratePLN;
+    private String rateBGN;
+    private String rateTRY;
+    private String rateCNY;
+    private String rateNOK;
+    private String rateNZD;
+    private String rateZAR;
+    private String rateMXN;
+    private String rateILS;
+    private String rateKRW;
+    private String rateMYR;
+    private String disclaimer;
 
-    public BitcoinAllCurrency(String disclaimer, String priceUSD, String priceEUR, String priceGBP) {
+    public BitcoinAllCurrency(String rateUSD, String rateEUR, String rateGBP, String rateCAD, String rateHKD,
+                              String rateISK, String ratePHP, String rateDKK, String rateHUF, String rateCZK,
+                              String rateAUD, String rateRON, String rateSEK, String rateIDR, String rateINR,
+                              String rateBRL, String rateRUB, String rateHRK, String rateJPY, String rateTHB,
+                              String rateCHF, String rateSGD, String ratePLN, String rateBGN, String rateTRY,
+                              String rateCNY, String rateNOK, String rateNZD, String rateZAR, String rateMXN,
+                              String rateILS, String rateKRW, String rateMYR, String disclaimer) {
+        this.rateUSD = rateUSD;
+        this.rateEUR = rateEUR;
+        this.rateGBP = rateGBP;
+        this.rateCAD = rateCAD;
+        this.rateHKD = rateHKD;
+        this.rateISK = rateISK;
+        this.ratePHP = ratePHP;
+        this.rateDKK = rateDKK;
+        this.rateHUF = rateHUF;
+        this.rateCZK = rateCZK;
+        this.rateAUD = rateAUD;
+        this.rateRON = rateRON;
+        this.rateSEK = rateSEK;
+        this.rateIDR = rateIDR;
+        this.rateINR = rateINR;
+        this.rateBRL = rateBRL;
+        this.rateRUB = rateRUB;
+        this.rateHRK = rateHRK;
+        this.rateJPY = rateJPY;
+        this.rateTHB = rateTHB;
+        this.rateCHF = rateCHF;
+        this.rateSGD = rateSGD;
+        this.ratePLN = ratePLN;
+        this.rateBGN = rateBGN;
+        this.rateTRY = rateTRY;
+        this.rateCNY = rateCNY;
+        this.rateNOK = rateNOK;
+        this.rateNZD = rateNZD;
+        this.rateZAR = rateZAR;
+        this.rateMXN = rateMXN;
+        this.rateILS = rateILS;
+        this.rateKRW = rateKRW;
+        this.rateMYR = rateMYR;
         this.disclaimer = disclaimer;
-        this.rateUSD = priceUSD;
-        this.rateEUR = priceEUR;
-        this.rateGBP = priceGBP;
-    }
-
-    public String getDisclaimer() {
-        return disclaimer;
     }
 
     public String getRateUSD() {
         return rateUSD;
     }
 
+    public void setRateUSD(String rateUSD) {
+        this.rateUSD = rateUSD;
+    }
+
     public String getRateEUR() {
         return rateEUR;
     }
 
+    public void setRateEUR(String rateEUR) {
+        this.rateEUR = rateEUR;
+    }
+
     public String getRateGBP() {
         return rateGBP;
+    }
+
+    public void setRateGBP(String rateGBP) {
+        this.rateGBP = rateGBP;
+    }
+
+    public String getRateCAD() {
+        return rateCAD;
+    }
+
+    public void setRateCAD(String rateCAD) {
+        this.rateCAD = rateCAD;
+    }
+
+    public String getRateHKD() {
+        return rateHKD;
+    }
+
+    public void setRateHKD(String rateHKD) {
+        this.rateHKD = rateHKD;
+    }
+
+    public String getRateISK() {
+        return rateISK;
+    }
+
+    public void setRateISK(String rateISK) {
+        this.rateISK = rateISK;
+    }
+
+    public String getRatePHP() {
+        return ratePHP;
+    }
+
+    public void setRatePHP(String ratePHP) {
+        this.ratePHP = ratePHP;
+    }
+
+    public String getRateDKK() {
+        return rateDKK;
+    }
+
+    public void setRateDKK(String rateDKK) {
+        this.rateDKK = rateDKK;
+    }
+
+    public String getRateHUF() {
+        return rateHUF;
+    }
+
+    public void setRateHUF(String rateHUF) {
+        this.rateHUF = rateHUF;
+    }
+
+    public String getRateCZK() {
+        return rateCZK;
+    }
+
+    public void setRateCZK(String rateCZK) {
+        this.rateCZK = rateCZK;
+    }
+
+    public String getRateAUD() {
+        return rateAUD;
+    }
+
+    public void setRateAUD(String rateAUD) {
+        this.rateAUD = rateAUD;
+    }
+
+    public String getRateRON() {
+        return rateRON;
+    }
+
+    public void setRateRON(String rateRON) {
+        this.rateRON = rateRON;
+    }
+
+    public String getRateSEK() {
+        return rateSEK;
+    }
+
+    public void setRateSEK(String rateSEK) {
+        this.rateSEK = rateSEK;
+    }
+
+    public String getRateIDR() {
+        return rateIDR;
+    }
+
+    public void setRateIDR(String rateIDR) {
+        this.rateIDR = rateIDR;
+    }
+
+    public String getRateINR() {
+        return rateINR;
+    }
+
+    public void setRateINR(String rateINR) {
+        this.rateINR = rateINR;
+    }
+
+    public String getRateBRL() {
+        return rateBRL;
+    }
+
+    public void setRateBRL(String rateBRL) {
+        this.rateBRL = rateBRL;
+    }
+
+    public String getRateRUB() {
+        return rateRUB;
+    }
+
+    public void setRateRUB(String rateRUB) {
+        this.rateRUB = rateRUB;
+    }
+
+    public String getRateHRK() {
+        return rateHRK;
+    }
+
+    public void setRateHRK(String rateHRK) {
+        this.rateHRK = rateHRK;
+    }
+
+    public String getRateJPY() {
+        return rateJPY;
+    }
+
+    public void setRateJPY(String rateJPY) {
+        this.rateJPY = rateJPY;
+    }
+
+    public String getRateTHB() {
+        return rateTHB;
+    }
+
+    public void setRateTHB(String rateTHB) {
+        this.rateTHB = rateTHB;
+    }
+
+    public String getRateCHF() {
+        return rateCHF;
+    }
+
+    public void setRateCHF(String rateCHF) {
+        this.rateCHF = rateCHF;
+    }
+
+    public String getRateSGD() {
+        return rateSGD;
+    }
+
+    public void setRateSGD(String rateSGD) {
+        this.rateSGD = rateSGD;
+    }
+
+    public String getRatePLN() {
+        return ratePLN;
+    }
+
+    public void setRatePLN(String ratePLN) {
+        this.ratePLN = ratePLN;
+    }
+
+    public String getRateBGN() {
+        return rateBGN;
+    }
+
+    public void setRateBGN(String rateBGN) {
+        this.rateBGN = rateBGN;
+    }
+
+    public String getRateTRY() {
+        return rateTRY;
+    }
+
+    public void setRateTRY(String rateTRY) {
+        this.rateTRY = rateTRY;
+    }
+
+    public String getRateCNY() {
+        return rateCNY;
+    }
+
+    public void setRateCNY(String rateCNY) {
+        this.rateCNY = rateCNY;
+    }
+
+    public String getRateNOK() {
+        return rateNOK;
+    }
+
+    public void setRateNOK(String rateNOK) {
+        this.rateNOK = rateNOK;
+    }
+
+    public String getRateNZD() {
+        return rateNZD;
+    }
+
+    public void setRateNZD(String rateNZD) {
+        this.rateNZD = rateNZD;
+    }
+
+    public String getRateZAR() {
+        return rateZAR;
+    }
+
+    public void setRateZAR(String rateZAR) {
+        this.rateZAR = rateZAR;
+    }
+
+    public String getRateMXN() {
+        return rateMXN;
+    }
+
+    public void setRateMXN(String rateMXN) {
+        this.rateMXN = rateMXN;
+    }
+
+    public String getRateILS() {
+        return rateILS;
+    }
+
+    public void setRateILS(String rateILS) {
+        this.rateILS = rateILS;
+    }
+
+    public String getRateKRW() {
+        return rateKRW;
+    }
+
+    public void setRateKRW(String rateKRW) {
+        this.rateKRW = rateKRW;
+    }
+
+    public String getRateMYR() {
+        return rateMYR;
+    }
+
+    public void setRateMYR(String rateMYR) {
+        this.rateMYR = rateMYR;
+    }
+
+    public String getDisclaimer() {
+        return disclaimer;
+    }
+
+    public void setDisclaimer(String disclaimer) {
+        this.disclaimer = disclaimer;
     }
 }
