57f32aa1cf64acb04b8a86ffb0fcb4676b2e4180
john monson <johnmonson@johns-MacBook-Pro.local>
2020-06-02 09:33:48 -0700

    Created Currency Exchange POJO

---
+++ src/main/java/com/restservice/POJO/CurrencyExchange.java
@@ -0,0 +1,313 @@
+package com.restservice.POJO;
+
+public class CurrencyExchange {
+    private double CAD;
+    private double HKD;
+    private double ISK;
+    private double PHP;
+    private double DKK;
+    private double HUF;
+    private double CZK;
+    private double AUD;
+    private double RON;
+    private double SEK;
+    private double IDR;
+    private double INR;
+    private double BRL;
+    private double RUB;
+    private double HRK;
+    private double JPY;
+    private double THB;
+    private double CHF;
+    private double SGD;
+    private double PLN;
+    private double BGN;
+    private double TRY;
+    private double CNY;
+    private double NOK;
+    private double NZD;
+    private double ZAR;
+    private double MXN;
+    private double ILS;
+    private double KRW;
+    private double MYR;
+
+    public CurrencyExchange(double CAD, double HKD, double ISK, double PHP, double DKK, double HUF, double CZK,
+                            double AUD, double RON, double SEK, double IDR, double INR, double BRL, double RUB,
+                            double HRK, double JPY, double THB, double CHF, double SGD, double PLN, double BGN,
+                            double TRY, double CNY, double NOK, double NZD, double ZAR, double MXN, double ILS,
+                            double KRW, double MYR) {
+        this.CAD = CAD;
+        this.HKD = HKD;
+        this.ISK = ISK;
+        this.PHP = PHP;
+        this.DKK = DKK;
+        this.HUF = HUF;
+        this.CZK = CZK;
+        this.AUD = AUD;
+        this.RON = RON;
+        this.SEK = SEK;
+        this.IDR = IDR;
+        this.INR = INR;
+        this.BRL = BRL;
+        this.RUB = RUB;
+        this.HRK = HRK;
+        this.JPY = JPY;
+        this.THB = THB;
+        this.CHF = CHF;
+        this.SGD = SGD;
+        this.PLN = PLN;
+        this.BGN = BGN;
+        this.TRY = TRY;
+        this.CNY = CNY;
+        this.NOK = NOK;
+        this.NZD = NZD;
+        this.ZAR = ZAR;
+        this.MXN = MXN;
+        this.ILS = ILS;
+        this.KRW = KRW;
+        this.MYR = MYR;
+    }
+
+    public double getCAD() {
+        return CAD;
+    }
+
+    public void setCAD(double CAD) {
+        this.CAD = CAD;
+    }
+
+    public double getHKD() {
+        return HKD;
+    }
+
+    public void setHKD(double HKD) {
+        this.HKD = HKD;
+    }
+
+    public double getISK() {
+        return ISK;
+    }
+
+    public void setISK(double ISK) {
+        this.ISK = ISK;
+    }
+
+    public double getPHP() {
+        return PHP;
+    }
+
+    public void setPHP(double PHP) {
+        this.PHP = PHP;
+    }
+
+    public double getDKK() {
+        return DKK;
+    }
+
+    public void setDKK(double DKK) {
+        this.DKK = DKK;
+    }
+
+    public double getHUF() {
+        return HUF;
+    }
+
+    public void setHUF(double HUF) {
+        this.HUF = HUF;
+    }
+
+    public double getCZK() {
+        return CZK;
+    }
+
+    public void setCZK(double CZK) {
+        this.CZK = CZK;
+    }
+
+    public double getAUD() {
+        return AUD;
+    }
+
+    public void setAUD(double AUD) {
+        this.AUD = AUD;
+    }
+
+    public double getRON() {
+        return RON;
+    }
+
+    public void setRON(double RON) {
+        this.RON = RON;
+    }
+
+    public double getSEK() {
+        return SEK;
+    }
+
+    public void setSEK(double SEK) {
+        this.SEK = SEK;
+    }
+
+    public double getIDR() {
+        return IDR;
+    }
+
+    public void setIDR(double IDR) {
+        this.IDR = IDR;
+    }
+
+    public double getINR() {
+        return INR;
+    }
+
+    public void setINR(double INR) {
+        this.INR = INR;
+    }
+
+    public double getBRL() {
+        return BRL;
+    }
+
+    public void setBRL(double BRL) {
+        this.BRL = BRL;
+    }
+
+    public double getRUB() {
+        return RUB;
+    }
+
+    public void setRUB(double RUB) {
+        this.RUB = RUB;
+    }
+
+    public double getHRK() {
+        return HRK;
+    }
+
+    public void setHRK(double HRK) {
+        this.HRK = HRK;
+    }
+
+    public double getJPY() {
+        return JPY;
+    }
+
+    public void setJPY(double JPY) {
+        this.JPY = JPY;
+    }
+
+    public double getTHB() {
+        return THB;
+    }
+
+    public void setTHB(double THB) {
+        this.THB = THB;
+    }
+
+    public double getCHF() {
+        return CHF;
+    }
+
+    public void setCHF(double CHF) {
+        this.CHF = CHF;
+    }
+
+    public double getSGD() {
+        return SGD;
+    }
+
+    public void setSGD(double SGD) {
+        this.SGD = SGD;
+    }
+
+    public double getPLN() {
+        return PLN;
+    }
+
+    public void setPLN(double PLN) {
+        this.PLN = PLN;
+    }
+
+    public double getBGN() {
+        return BGN;
+    }
+
+    public void setBGN(double BGN) {
+        this.BGN = BGN;
+    }
+
+    public double getTRY() {
+        return TRY;
+    }
+
+    public void setTRY(double TRY) {
+        this.TRY = TRY;
+    }
+
+    public double getCNY() {
+        return CNY;
+    }
+
+    public void setCNY(double CNY) {
+        this.CNY = CNY;
+    }
+
+    public double getNOK() {
+        return NOK;
+    }
+
+    public void setNOK(double NOK) {
+        this.NOK = NOK;
+    }
+
+    public double getNZD() {
+        return NZD;
+    }
+
+    public void setNZD(double NZD) {
+        this.NZD = NZD;
+    }
+
+    public double getZAR() {
+        return ZAR;
+    }
+
+    public void setZAR(double ZAR) {
+        this.ZAR = ZAR;
+    }
+
+    public double getMXN() {
+        return MXN;
+    }
+
+    public void setMXN(double MXN) {
+        this.MXN = MXN;
+    }
+
+    public double getILS() {
+        return ILS;
+    }
+
+    public void setILS(double ILS) {
+        this.ILS = ILS;
+    }
+
+    public double getKRW() {
+        return KRW;
+    }
+
+    public void setKRW(double KRW) {
+        this.KRW = KRW;
+    }
+
+    public double getMYR() {
+        return MYR;
+    }
+
+    public void setMYR(double MYR) {
+        this.MYR = MYR;
+    }
+}
+
+
